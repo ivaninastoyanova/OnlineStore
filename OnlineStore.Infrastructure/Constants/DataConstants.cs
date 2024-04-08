7c87aba3ebf2a1faac31b1c2a4f498eec1339aca
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-08 20:46:51 +0300

    Data constants added

---
+++ OnlineStore.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Infrastructure.Constants
+{
+    public class DataConstants
+    {
+        //Comic Constants
+        public const int ComicTitleMinLength = 5;
+        public const int ComicTitleMaxLength = 50;
+
+        public const int ComicDescriptionMinLength = 10;
+        public const int ComicDescriptionMaxLength = 1000;
+
+        public const decimal ComicPriceMin = 0.01m;
+
+        //Creator Constants
+        public const int CreatorNameMinLength = 5;
+        public const int CreatorNameMaxLength = 50;
+
+        public const int CreatorBiographyMinLength = 5;
+        public const int CreatorBiographyMaxLength = 1000;
+
+        //Category Constants
+        public const int CategoryNameMinLength = 3;
+        public const int CategoryNameMaxLength = 30;
+
+        //Review Constants
+        public const int ReviewTextMinLength = 2;
+        public const int ReviewTextMaxLength = 150;
+
+        public const int ReviewStarRatingMin = 1;
+        public const int ReviewStarRatingMax = 5;
+
+        //Photo Constants
+        public const int PhotoUrlMaxLength = 3000;
+    }
+}
