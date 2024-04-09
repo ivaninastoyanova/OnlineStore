1d74e872b44177ccd15d4426e14a36bf0c8758fd
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 16:20:52 +0300

    Comic controller added and started implementation. All method added.

---
+++ OnlineStore/Controllers/ComicController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Core.Models.Comic;
+
+namespace OnlineStore.Controllers
+{
+    public class ComicController : BaseController
+    {
+        private ICreatorService creatorService;
+        private IComicService comicService;
+        private ICategoryService categoryService;
+
+        public ComicController(ICreatorService creatorService,
+                    IComicService comicService, 
+                    ICategoryService categoryService)
+        {
+            this.creatorService = creatorService;
+            this.comicService = comicService;
+            this.categoryService = categoryService;
+
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> All([FromQuery] ComicAllQueryModel query)
+        {
+            AllComicsFilteredAndOrdered model = await comicService.AllAsync(query);
+
+            query.Comics = model.Comics;
+            query.TotalComics = model.TotalComicsCount;
+            query.Categories = await categoryService.AllCategoryNames();
+
+            return View(query);
+        }
+    }
+}
