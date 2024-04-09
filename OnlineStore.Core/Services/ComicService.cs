a639a85a5ebc00e93043c9ab09f9175749b85a4f
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 16:16:11 +0300

    Comic service added and started implementing functionalities for the All Comics page. Implemented pagination, search and filtering.

---
+++ OnlineStore.Core/Services/ComicService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Core.Models.Comic;
+using OnlineStore.Core.Models.Comic.Enums;
+using OnlineStore.Infrastructure;
+using OnlineStore.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Services
+{
+    public class ComicService : IComicService
+    {
+        private OnlineStoreDbContext db;
+
+        public ComicService(OnlineStoreDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task AddComicAsync(AddComicViewModel model, Creator creator)
+        {
+            Comic comic = new Comic
+            {
+                Id = model.Id,
+                Title = model.Title,
+                Description = model.Description,
+                CreatorId = creator.Id,
+                CategoryId = int.Parse(model.CategoryId),
+                Price = model.Price,
+                PhotoUrl = model.PhotoUrl
+            };
+
+            await db.Comics.AddAsync(comic);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<AllComicsFilteredAndOrdered> AllAsync(ComicAllQueryModel queryModel)
+        {
+            IQueryable<Comic> comicQuery = this.db
+                                .Comics
+                                .Where(b => b.IsDeleted == false)
+                                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(queryModel.CategoryName))
+            {
+                comicQuery = comicQuery
+                    .Where(h => h.Category.Name == queryModel.CategoryName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(queryModel.SearchString))
+            {
+                string wildCard = $"%{queryModel.SearchString.ToLower()}%";
+
+                comicQuery = comicQuery
+                    .Where(h => EF.Functions.Like(h.Title, wildCard) ||
+                                EF.Functions.Like(h.Creator.FullName, wildCard) ||
+                                EF.Functions.Like(h.Description, wildCard));
+            }
+
+            comicQuery = queryModel.ComicSorting switch
+            {
+                ComicSortEnum.AlphabeticallyAscending => comicQuery
+                    .OrderBy(b => b.Title),
+                ComicSortEnum.AlphabeticallyDescending => comicQuery
+                    .OrderByDescending(b => b.Title),
+                ComicSortEnum.PriceAscending => comicQuery
+                    .OrderBy(b => b.Price),
+                ComicSortEnum.PriceDescending => comicQuery
+                    .OrderByDescending(b => b.Price),
+                ComicSortEnum.ByCreatorDescending => comicQuery
+                    .OrderByDescending(b => b.Creator.FullName),
+                ComicSortEnum.ByCreatorAscending => comicQuery
+                    .OrderBy(b => b.Creator.FullName),
+                _ => comicQuery
+                    .OrderBy(b => b.Title)
+            };
+
+            IEnumerable<ComicAllViewModel> comics = await comicQuery
+                .Skip((queryModel.CurrentPage - 1) * queryModel.ComicsPerPage)
+                .Take(queryModel.ComicsPerPage) 
+                .Select(b => new ComicAllViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Description = b.Description,
+                    PhotoUrl = b.PhotoUrl,
+                    Price = b.Price,
+                    CreatorName = b.Creator.FullName,
+                    CategoryName = b.Category.Name,
+                    IsDeleted = b.IsDeleted
+                })
+                .ToArrayAsync();
+
+            int totalComicsCount = comicQuery.Count();
+
+            return new AllComicsFilteredAndOrdered()
+            {
+                TotalComicsCount = totalComicsCount,
+                Comics = comics
+            };
+        }
+    }
+}
