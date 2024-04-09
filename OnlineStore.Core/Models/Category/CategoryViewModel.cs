bca7d8e901f32817730eddf7558b2f6d2dd2368b
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 18:31:39 +0300

    Category view model added

---
+++ OnlineStore.Core/Models/Category/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using OnlineStore.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OnlineStore.Infrastructure.Constants.DataConstants;
+
+namespace OnlineStore.Core.Models.Category
+{
+    public class CategoryViewModel
+    {
+        public int CategoryId { get; set; }
+
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength)]
+        public string Name { get; set; } = null!;
+    }
+}
