03ca6b528955d750502c868e94af605008c1fc0b
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 16:04:31 +0300

    Added categories sorting enum

---
+++ OnlineStore.Core/Models/Comic/Enums/ComicSortEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Models.Comic.Enums
+{
+    public enum ComicSortEnum
+    {
+        PriceAscending,
+        PriceDescending,
+        AlphabeticallyAscending,
+        AlphabeticallyDescending,
+        ByCreatorAscending,
+        ByCreatorDescending
+    }
+}
