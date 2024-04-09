13dd312afd5a8fa3744510a69c38038de5297fa9
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 16:09:23 +0300

    Created Category and Creator controllers

---
+++ OnlineStore/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
