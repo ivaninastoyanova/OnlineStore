eb9db19f026828e6f6d1f04df056a313277f3884
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-09 16:07:56 +0300

    Created Category and Creator services and started the implementation

---
+++ OnlineStore.Core/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Core.Models.Category;
+using OnlineStore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private OnlineStoreDbContext db;
+
+        public CategoryService(OnlineStoreDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync()
+        {
+            return await db.Categories
+                .Select(c => new CategoryViewModel
+                {
+                    CategoryId = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<string>> AllCategoryNames()
+        {
+            return await db.Categories
+                .Select(c => c.Name)
+                .ToListAsync();
+        }
+    }
+}
---
+++ OnlineStore.Core/Services/CreatorService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Infrastructure;
+using OnlineStore.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Services
+{
+    public class CreatorService : ICreatorService
+    {
+        private OnlineStoreDbContext db;
+
+        public CreatorService(OnlineStoreDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Creator> GetCreatorByNameAsync(string name)
+        {
+            Creator? creator = await db.Creators
+                            .FirstOrDefaultAsync(c => c.FullName == name);
+
+            if (creator == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return creator;
+        }
+    }
+}
