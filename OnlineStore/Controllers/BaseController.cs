3d740aff8289cab6c41853ce68d8aa29812cc273
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-11 12:18:15 +0300

    Implemented methods to get info fot the current user.

--- OnlineStore/Controllers/BaseController.cs
+++ OnlineStore/Controllers/BaseController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace OnlineStore.Controllers
 {
     [Authorize]
     public class BaseController : Controller
     {
-       
+        public static string? GetId(ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static string? GetEmail(ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.Email);
+        }
     }
 }
