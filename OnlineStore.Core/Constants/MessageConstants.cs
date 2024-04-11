b9aaa63b2ae58a262df1301fe3984e7ca7822f70
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-11 21:28:22 +0300

    Toastr notification messages implemented.

---
+++ OnlineStore.Core/Constants/MessageConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Constants
+{
+    public class MessageConstants
+    {
+        public const string UserMessageSuccess = "UserMessageSuccess";
+
+        public const string UserMessageError = "UserMessageError";
+    }
+}
