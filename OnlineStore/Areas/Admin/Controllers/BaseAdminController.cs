39f6da0657ac137b1ff8f11c1f29251569a5bd16
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-12 16:48:56 +0300

    Created BaseAdminController. Created Home controller and home page for the admin area.

---
+++ OnlineStore/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using static OnlineStore.Infrastructure.Constants.AdministratorConstants;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+
+    [Area(AdminAreaName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class BaseAdminController : Controller
+    {
+
+    }
+}
---
+++ OnlineStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    public class HomeController : BaseAdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
