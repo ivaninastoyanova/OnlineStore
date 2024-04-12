83beec159d911410ca9813390e6cf66a28c2bf45
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-12 16:54:47 +0300

    Implemented page where admin users can see all registered users.

---
+++ OnlineStore/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Infrastructure.Constants;
+using OnlineStore.Infrastructure.Data.Models;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    public class UserController : BaseAdminController
+    {
+        private readonly IUserService userService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(
+            IUserService _userService,
+            UserManager<ApplicationUser> userManager)
+        {
+            userService = _userService;
+            this.userManager = userManager;
+        }
+
+        [Route("Admin/User/All")]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<ApplicationUser> users = await userService.GetUsers();
+
+            foreach (ApplicationUser user in users)
+            {
+                if (await userManager.IsInRoleAsync(user, AdministratorConstants.AdminRoleName))
+                {
+                    user.IsAdmin = true;
+                }
+            }
+
+            return this.View(users);
+        }
+    }
+}
