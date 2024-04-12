4107dab4e2531ac1337370797fd8e4b5bc4a42f2
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-12 16:53:14 +0300

    Created a IUserService contract and the UserService itself - will be used by admins.

---
+++ OnlineStore.Core/Contracts/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStore.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Contracts
+{
+    public  interface IUserService
+    {
+        public Task<IEnumerable<ApplicationUser>> GetUsers();
+
+        public Task<ApplicationUser> GetUserById(Guid id);
+    }
+}
---
+++ OnlineStore.Core/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Contracts;
+using OnlineStore.Infrastructure;
+using OnlineStore.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Services
+{
+    public class UserService : IUserService
+    {
+        private OnlineStoreDbContext db;
+
+        public UserService(OnlineStoreDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<ApplicationUser> GetUserById(Guid id)
+        {
+            return await db.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetUsers()
+        {
+            IEnumerable<ApplicationUser> users = await db.Users.ToListAsync();
+
+            return users;
+        }
+    }
+}
