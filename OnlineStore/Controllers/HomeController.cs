fec174a2275b38f04be79a98e2e5008ea3c4605e
Ivanina Stoyanova <ivoka674@abv.bg>
2024-04-14 12:40:45 +0300

    Customer error pages added.

--- OnlineStore/Controllers/HomeController.cs
+++ OnlineStore/Controllers/HomeController.cs
@@ -26,10 +26,26 @@ namespace OnlineStore.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+
+            if (statusCode == 500)
+            {
+                return View("Error500");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
